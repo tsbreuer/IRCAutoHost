b3ee05d7b730189dbb7974137773702fe1214886
tsbreuer <tsbreuer@hotmail.com>
2016-12-18 17:45:49 -0300

    Cleaning

--- src/autohost/Slot.java
+++
@@ -1,8 +0,0 @@
-package autohost;
-
-public class Slot {
-	public int id;
-	public int playerid;
-	public String name;
-	public String status;
-}
--- src/autohost/utils/IRCListener.java
+++
@@ -1,9 +0,0 @@
-package autohost.utils;
-
-
-public class IRCListener  {
-
-        public static void main(String[] args) throws Exception {
-
-        }
-}
