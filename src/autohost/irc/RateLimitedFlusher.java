277163cd3dd08b808eccac8b76f2d2fffc659978
tsbreuer <tsbreuer@gmail.com>
2017-10-23 15:50:44 -0300

    Attempt to fix concurrentModificationException error on the message flusher

--- src/autohost/irc/RateLimitedFlusher.java
+++ src/autohost/irc/RateLimitedFlusher.java
@@ -1,12 +1,13 @@
 package autohost.irc;
 
+import java.util.ConcurrentModificationException;
 import java.util.Iterator;
 
 import autohost.util.ThreadUtils;
 
 class RateLimitedFlusher extends Thread {
 	private IRCClient m_client;
-	private int       m_delay;
+	private int m_delay;
 
 	RateLimitedFlusher(IRCClient client, int delay) {
 		m_client = client;
@@ -15,15 +16,21 @@ class RateLimitedFlusher extends Thread {
 
 	@Override
 	public void run() {
-		while (true) {
-			Iterator<RateLimitedChannel> it = m_client.getChannels().values().iterator();
-			while (it.hasNext()) {
-				RateLimitedChannel limiter = it.next();
-				String line = limiter.poll();
-				if (line != null)
-				m_client.write(line);
+		try {
+			while (true) {
+				Iterator<RateLimitedChannel> it = m_client.getChannels().values().iterator();
+				synchronized (it) {
+					while (it.hasNext()) {
+						RateLimitedChannel limiter = it.next();
+						String line = limiter.poll();
+						if (line != null)
+							m_client.write(line);
+					}
+					ThreadUtils.sleepQuietly(m_delay);
+				}
 			}
-			ThreadUtils.sleepQuietly(m_delay);
+		} catch (ConcurrentModificationException e) {
+			e.printStackTrace();
 		}
 	}
 }
